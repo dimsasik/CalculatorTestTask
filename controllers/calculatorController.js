b15a9de16237dfceb5ae3afa04f256f1dbe07f59
dimsasik <74226449+dimsasik@users.noreply.github.com>
2023-05-13 08:39:39 +0400

    Update calculatorController.js

--- controllers/calculatorController.js
+++ controllers/calculatorController.js
@@ -1,18 +1,21 @@
 const calculatorModel = require('../models/calculatorModel');
 
-
-const createOperationHandler = (operation) => (req, res) => {
-    const num1 = Number(req.query.num1);
-    const num2 = Number(req.query.num2);
-    try {
-        const result = calculatorModel[operation](num1, num2);
-        res.render('index',{'result': result});
-    } catch (error) {
-        res.status(400).send(error.message);
-    }
+const createOperationHandler = (operation) => {
+    return (req, res) => {
+        const num1 = Number(req.query['num1']);
+        const num2 = Number(req.query['num2']);
+        try {
+            const result = calculatorModel[operation](num1, num2);
+            res.json(result);
+        } catch (error) {
+            res.status(400).send(error.message);
+        }
+    };
 };
 
-exports.add = createOperationHandler('add');
-exports.subtract = createOperationHandler('subtract');
-exports.multiply = createOperationHandler('multiply');
-exports.divide = createOperationHandler('divide');
+module.exports = {
+    add: createOperationHandler('add'),
+    subtract: createOperationHandler('subtract'),
+    multiply: createOperationHandler('multiply'),
+    divide: createOperationHandler('divide')
+};
