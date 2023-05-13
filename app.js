fec13ca278aeeaf3ecc8568224d9ffec44b889a4
dimsasik <74226449+dimsasik@users.noreply.github.com>
2023-05-13 08:38:12 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const calculatorController = require('./controllers/calculatorController');
-
+const bodyParser = require('body-parser');
 const app = express();
-app.set('view engine','pug');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.get('/add', calculatorController.add);
 app.get('/subtract', calculatorController.subtract);
 app.get('/multiply', calculatorController.multiply);
 app.get('/divide', calculatorController.divide);
-
 
 app.listen(3000, () => {
     console.log('Calculator app listening on port 3000');
